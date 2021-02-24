f9d1657922aa92692946bd99df05f3fa87b94159
Alejandra Bejarano Alarcon <alejandrabejarano6@gmail.com>
2021-02-24 19:05:41 +0100

    url get

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function getProducts() {
             productsContainer +=
             `<div class="col-sm-6 col-lg-4 mb-3 d-flex">
                 <div class="card card-flex">
-                    <a href="#" class="product-preview"><img class="card-img-top" src="${teddie.imageUrl}"></a>
+                    <a href="productPreview.html?id=${teddie._id}" class="product-preview"><img class="card-img-top" src="${teddie.imageUrl}"></a>
                     <div class="card-body">
                         <a href="#" class="product-preview color-link"><h3 class="card-title text-center">${teddie.name}</h3></a>
                         <p class="d-none">${teddie._id}</p>
