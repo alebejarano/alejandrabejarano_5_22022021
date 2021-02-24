d0e3d16010e4a68998f77e30da21bb7f3eeb721f
Alejandra Bejarano Alarcon <alejandrabejarano6@gmail.com>
2021-02-24 11:31:04 +0100

    js get products

--- main.js
+++ main.js
@@ -3,20 +3,18 @@ function getProducts() {
     fetch('http://localhost:3000/api/teddies')
     .then((res) => res.json())
     .then((data) => {
-        let productsContainer = '<h2 class="teddies-title text-center my-5">Notre Teddies</h2>';
+        let productsContainer = '';
         data.forEach(function(teddie){
             productsContainer +=
-            `<div class="row">
-                <div class="col-lg-3">
-                    <div class="card">
-                    <img class="card-img-top" src="${teddie.imageUrl}">
-                        <div class="card-body">
-                            <h3 class="card-title">${teddie.name}</h3>
-                            <p class="id-productsList">${teddie._id}</p>
-                            <p class="card-text">${teddie.price}&euro;</p>
-                        </div> 
-                    </div>       
-                </div>
+            `<div class="col-sm-6 col-lg-4 mb-3">
+                <div class="card">
+                <img class="card-img-top" src="${teddie.imageUrl}">
+                    <div class="card-body">
+                        <h3 class="card-title">${teddie.name}</h3>
+                        <p class="id-productsList">${teddie._id}</p>
+                        <p class="card-text">${teddie.price}&euro;</p>
+                    </div> 
+                </div>       
             </div>
             `;
         });
