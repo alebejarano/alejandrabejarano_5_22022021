da7611f02a8da5e63bee66918488d2ea3d019fbe
Alejandra Bejarano Alarcon <alejandrabejarano6@gmail.com>
2021-02-25 20:00:07 +0100

    styling preview page and nav

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ function getProducts() {
                     <div class="card card-flex">
                         <img class="card-img-top" src="${teddie.imageUrl}" alt="ours en peluche">
                         <div class="card-body">
-                            <p class="card-title text-center">${teddie.name}</p>
+                            <p class="card-title text-center color-link">${teddie.name}</p>
                             <p class="d-none">${teddie._id}</p>
-                            <p class="card-text text-center">${teddie.price}&euro;</p>
+                            <p class="card-text text-center color-link">${teddie.price}&euro;</p>
                         </div> 
                     </div> 
                 </a>          
@@ -33,18 +33,18 @@ function getSingleProduct() {
     .then((res) => res.json())
     .then((teddie) => {
         let preview = `
-        <div class="card mb-3" style="max-width: 540px;">
+        <div class="card mb-3">
             <div class="row no-gutters">
                 <div class="col-md-4">
                     <img  class="card-img" src="${teddie.imageUrl}" alt="ours en peluche">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">${teddie.name}</h5>
+                        <h5 class="card-title teddie-name">${teddie.name}</h5>
                         <p class="card-text">${teddie.description}</p>
                         <p class="card-text">${teddie.price}&euro;</p>
-                        <p class="card-text"><small class="text-muted">${teddie.colors}</small></p>
-                        <button type="button" class="btn product-preview">Ajouter au panier</button>
+                        <p class="card-text"><small class="text-muted">Choix des Coleurs: ${teddie.colors}</small></p>
+                        <button type="button" class="btn  btn-light  py-1 px-3 product-preview">AJOUTER AU PANIER</button>
                     </div>
                 </div>
             </div>
